bfe3c21930d8f323f517f16eca1664ea17fd9526
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-06-27 18:52:41 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def using_pop_with_args(array, string)
+  array.pop(string)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def using_shift_with_args(array, string)
+  array.shift(string)
+end
